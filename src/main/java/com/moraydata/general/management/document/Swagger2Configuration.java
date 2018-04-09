18f833df007ee816b7b4dfb64367253ff602d15e
beamofsoul <beamofsoul@sina.com>
2018-04-09 19:25:32 +0800

    make codes structure clear

--- src/main/java/com/moraydata/general/management/document/Swagger2Configuration.java
+++ src/main/java/com/moraydata/general/management/document/Swagger2Configuration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.annotations.ApiOperation;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
